ac27f5c8a84d10e79ff1fbc61b68f71652faabbe
Pawar Pratiksha <patupawar2021@gmailcom>
2025-12-30 16:51:02 +0530

    inital

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,34 @@
 import { Routes } from '@angular/router';
+import { Dashbord } from './Pages/dashbord/dashbord';
+import { Members } from './Pages/members/members';
+import { Satff } from './Pages/satff/satff';
+import { Equipment } from './Pages/equipment/equipment';
+import { Finace } from './Pages/finace/finace';
+import { Reports } from './Pages/reports/reports';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: 'Dashboard',
+        component : Dashbord
+    },
+     {
+        path: 'MembersPage',
+        component : Members
+    },
+     {
+        path: 'staffPage',
+        component : Satff
+    },
+     {
+        path: 'Equipments',
+        component : Equipment
+    },
+     {
+        path: 'finances',
+        component : Finace
+    },
+    {
+        path : 'reports',
+        component :Reports
+    }
+];
--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,14 @@
 import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterLink, RouterOutlet } from '@angular/router';
+import { Dashbord } from './Pages/dashbord/dashbord';
+import { Reports } from './Pages/reports/reports';
+import { Equipment } from './Pages/equipment/equipment';
+import { Satff } from './Pages/satff/satff';
+import { Members } from './Pages/members/members';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,Dashbord,Reports,Equipment,Satff,Members,RouterLink],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
