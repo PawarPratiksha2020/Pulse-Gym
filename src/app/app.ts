f0efc04fae2d715f76d281a3abb4b49d5a03a202
Pawar Pratiksha <patupawar2021@gmailcom>
2026-01-13 17:34:59 +0530

    new change

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,26 +1,18 @@
 import { Component, OnInit, signal } from '@angular/core';
 import { RouterLink, RouterOutlet } from '@angular/router';
-import { Dashbord } from './Pages/dashbord/dashbord';
-import { Reports } from './Pages/reports/reports';
-import { EquipmentComponent } from './Pages/equipment/equipment';
-import { Satff } from './Pages/satff/satff';
-import { Members } from './Pages/members/members';
-
-import { Users } from './Pages/users/users';
-import { Dashboardheader } from "./Pages/dashbord/dashboardheader/dashboardheader";
-import { Grid } from './Pages/grid/grid';
+import { Layout } from 'layout';
 import { ThemeService } from './Pages/Services/theme-service';
-
-
+import { Dashboardheader } from "./Pages/dashbord/dashboardheader/dashboardheader";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Dashbord, Reports, EquipmentComponent, Satff, Members, RouterLink, Users, Dashboardheader,Grid],
-  templateUrl: './app.html',
-  styleUrl: './app.css'
+  standalone: true,
+  imports: [RouterOutlet, Layout, Dashboardheader,RouterLink,],
+templateUrl: './app.html',
+  styleUrls: ['./app.css'],
 })
-export class App implements OnInit {
-  protected readonly title = signal('DashboardPortal');
+export class App  {
+   protected readonly title = signal('DashboardPortal');
 
   isSidebarOpen = false;
 
